9465e23512e360d50eb204b7764e2e4ef24951f7
Taft123456 <43589323+Taft123456@users.noreply.github.com>
2019-07-17 16:07:59 -0700

    Update whe024_lab11_part1.c

--- turnin/whe024_lab11_part1.c
+++ turnin/whe024_lab11_part1.c
@@ -63,6 +63,100 @@ unsigned char GetKeypadKey() {
     return('\0');
 }
 
+enum pauseButtonSM_States {pauseButton_wait, pauseButton_press, pauseButton_release};
+
+int pauseButtonSMTick(int state){
+	unsigned char press = GetKeypadKey();
+	switch(state){
+		case pauseButton_wait:
+			state = (press == '\0') ? pauseButton_wait: pauseButton_press;
+			break;
+		case pauseButton_press:
+			state = pauseButton_release;
+			break;
+		case pauseButton_release:
+			state = (press == '\0') ? pauseButton_wait: pauseButton_release;
+			break;
+		default:
+			state = pausebutton_wait;
+			break;
+	}
+	switch(state){
+		case pauseButton_wait:
+			break;
+		case pauseButton_press:
+			switch (press) {
+                		case '\0': 
+					PORTB = 0x1F; 
+					break; 
+                		case '1':
+					PORTB = 0x01; 
+					break; 
+                		case '2': 
+					PORTB = 0x02; 
+					break;
+                		case '3': 
+					PORTB = 0x03; 
+					break;
+                		case '4': 
+					PORTB = 0x04; 
+					break;
+                		case '5': 
+					PORTB = 0x05; 
+					break;
+                		case '6': 
+					PORTB = 0x06; 
+					break;
+                		case '7': 
+					PORTB = 0x07; 
+					break;
+                		case '8': 
+					PORTB = 0x08; 
+					break;
+                		case '9': 
+					PORTB = 0x09; 
+					break;
+                		case 'A': 
+					PORTB = 0x0A; 
+					break;
+                		case 'B': 
+					PORTB = 0x0B; 
+					break;
+                		case 'C': 
+					PORTB = 0x0C; 
+					break;
+                		case 'D': 
+					PORTB = 0x0D; 
+					break;
+                		case '*': 
+					PORTB = 0x0E; 
+					break;
+                		case '0': 
+					PORTB = 0x00; 
+					break;
+                		case '#': 
+					PORTB = 0x0F; 
+					break;
+                		default: 
+					PORTB = 0x1B; 
+					braak;
+       			}
+			break;
+		case pauseButton_release:
+			break;
+		default:
+			break;
+	}
+	return state;
+}
+
+typedef struct _task{
+	signed char state; 		
+	unsigned long period; 		
+	unsigned long elapsedTime; 	
+	int (*TickFct)(int); 		
+} task;
+
 int main(void) {
     /* Insert DDR and PORT initializations */
     DDRB = 0xFF; 
@@ -70,64 +164,33 @@ int main(void) {
     PORTB = 0x00;
     PORTC = 0x0F;
     /* Insert your solution below */
-    unsigned char x;
+    static task task1;
+    task *tasks[] = {&task1};
+    const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
+	
+    task1.state = pauseButton_wait;
+    task1.period = 1;
+    task1.elapsedTime = task1.period;
+    task1.TickFct = &pauseButtonSMTick;
+    
+    unsigned long int GCD = 1;
+    TimerSet(1);
+    TimerOn();
+	
+    unsigned short i;
     while (1) {
-       x = GetKeypadKey();
-       switch (x) {
-                case '\0': 
-			PORTB = 0x1F; 
-			break; 
-                case '1':
-			PORTB = 0x01; 
-			break; 
-                case '2': 
-			PORTB = 0x02; 
-			break;
-                case '3': 
-			PORTB = 0x03; 
-			break;
-                case '4': 
-			PORTB = 0x04; 
-			break;
-                case '5': 
-			PORTB = 0x05; 
-			break;
-                case '6': 
-			PORTB = 0x06; 
-			break;
-                case '7': 
-			PORTB = 0x07; 
-			break;
-                case '8': 
-			PORTB = 0x08; 
-			break;
-                case '9': 
-			PORTB = 0x09; 
-			break;
-                case 'A': 
-			PORTB = 0x0A; 
-			break;
-                case 'B': 
-			PORTB = 0x0B; 
-			break;
-                case 'C': 
-			PORTB = 0x0C; 
-			break;
-                case 'D': 
-			PORTB = 0x0D; 
-			break;
-                case '*': 
-			PORTB = 0x0E; 
-			break;
-                case '0': 
-			PORTB = 0x00; 
-			break;
-                case '#': 
-			PORTB = 0x0F; 
-			break;
-                default: 
-			PORTB = 0x1B; 
-			break;
+       for ( i = 0; i < numTasks; i++ ) {
+			if ( tasks[i]->elapsedTime == tasks[i]->period )
+			{
+				tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
+				tasks[i]->elapsedTime = 0;
+			}
+			tasks[i]->elapsedTime += GCD;
+	       		GCD = findGCD(GCD, tasks[i]->period);
        }
+       
+       while (!TimerFlag);
+       TimerFlag = 0;
     }
+    return 1;
 }
