94227ad1b5ea631e2e6dcf189d63cc2c776e4040
Taft123456 <43589323+Taft123456@users.noreply.github.com>
2019-07-17 16:19:56 -0700

    Update main.c

--- source/main.c
+++ source/main.c
@@ -29,113 +29,113 @@ int pauseButtonSMTick(int state)
 {
   unsigned char press = GetKeypadKey();
   switch(state){
-   case pauseButton_wait:
-    state = (press == '\0') ? pauseButton_wait : pauseButton_press;
-    break;
-   case pauseButton_press
-    state = pauseButton_release;
-    break;
-   case pauseButton_release:
-    state = (press == '\0') ? pauseButton_wait : pauseButton_release;
-    break;
-   default:
-    state = pauseButton_wait;
-    break;
+    case pauseButton_wait:
+    	state = (press == '\0') ? pauseButton_wait : pauseButton_press;
+    	break;
+    case pauseButton_press
+    	state = pauseButton_release;
+    	break;
+    case pauseButton_release:
+    	state = (press == '\0') ? pauseButton_wait : pauseButton_release;
+    	break;
+    default:
+    	state = pauseButton_wait;
+    	break;
   }
   switch(state){
-  case pauseButton_wait:
+    case pauseButton_wait:
     break;
-  case pauseButton_press:
+    case pauseButton_press:
     switch(press){
-      case '\0':
-	PORTB = 0x1F;
-	break;
-      case '1': 
-	LCD_ClearScreen();
-        LCD_Cursor(1);
-	LCD_WriteData('1');
-	break;
-      case '2':
-        LCD_ClearScreen(); 
-        LCD_Cursor(1);
-        LCD_WriteData('2');
-        break;
-      case '3':
-        LCD_ClearScreen(); 
-        LCD_Cursor(1);
-        LCD_WriteData('3');
-        break;
-      case '4':
-        LCD_ClearScreen(); 
-        LCD_Cursor(1);
-        LCD_WriteData('4');
-        break;
-      case '5':
-        LCD_ClearScreen(); 
-        LCD_Cursor(1);
-        LCD_WriteData('5');
-        break;
-      case '6':
-        LCD_ClearScreen(); 
-        LCD_Cursor(1);
-        LCD_WriteData('6');
-        break;
-      case '7':
-        LCD_ClearScreen(); 
-        LCD_Cursor(1);
-        LCD_WriteData('7');
-        break;
-      case '8':
-        LCD_ClearScreen(); 
-        LCD_Cursor(1);
-        LCD_WriteData('8');
-        break;
-      case '9':
-        LCD_ClearScreen(); 
-        LCD_Cursor(1);
-        LCD_WriteData('9');
-        break;
-      case 'A':
-        LCD_ClearScreen(); 
-        LCD_Cursor(1);
-        LCD_WriteData('A');
-        break;
-      case 'B':
-        LCD_ClearScreen(); 
-        LCD_Cursor(1);
-        LCD_WriteData('B');
-        break;
-      case 'C':
-        LCD_ClearScreen(); 
-        LCD_Cursor(1);
-        LCD_WriteData('C');
-        break;
-      case 'D':
-        LCD_ClearScreen(); 
-        LCD_Cursor(1);
-        LCD_WriteData('D');
-        break;
-      case '*':
-        LCD_ClearScreen(); 
-        LCD_Cursor(1);
-        LCD_WriteData('*');
-        break;
-      case '#':
-        LCD_ClearScreen(); 
-        LCD_Cursor(1);
-        LCD_WriteData('#');
-        break;
-      case '0':
-        LCD_ClearScreen(); 
-        LCD_Cursor(1);
-        LCD_WriteData('0');
-        break;
-    }
-    break;
-  case pausebutton_release:
-    break;
-  default:
-    break;
+      	case '\0':
+		PORTB = 0x1F;
+		break;
+      	case '1': 
+		LCD_ClearScreen();
+        	LCD_Cursor(1);
+		LCD_WriteData('1');
+		break;
+      	case '2':
+        	LCD_ClearScreen(); 
+        	LCD_Cursor(1);
+        	LCD_WriteData('2');
+        	break;
+        case '3':
+        	LCD_ClearScreen(); 
+        	LCD_Cursor(1);
+        	LCD_WriteData('3');
+        	break;
+      	case '4':
+        	LCD_ClearScreen(); 
+        	LCD_Cursor(1);
+        	LCD_WriteData('4');
+        	break;
+      	case '5':
+        	LCD_ClearScreen(); 
+        	LCD_Cursor(1);
+        	LCD_WriteData('5');
+        	break;
+      	case '6':
+        	LCD_ClearScreen(); 
+        	LCD_Cursor(1);
+        	LCD_WriteData('6');
+        	break;
+        case '7':
+        	LCD_ClearScreen(); 
+        	LCD_Cursor(1);
+        	LCD_WriteData('7');
+        	break;
+      	case '8':
+        	LCD_ClearScreen(); 
+        	LCD_Cursor(1);
+        	LCD_WriteData('8');
+        	break;
+      	case '9':
+        	LCD_ClearScreen(); 
+        	LCD_Cursor(1);
+        	LCD_WriteData('9');
+        	break;
+      	case 'A':
+        	LCD_ClearScreen(); 
+        	LCD_Cursor(1);
+        	LCD_WriteData('A');
+        	break;
+      	case 'B':
+        	LCD_ClearScreen(); 
+        	LCD_Cursor(1);
+        	LCD_WriteData('B');
+        	break;
+      	case 'C':
+        	LCD_ClearScreen(); 
+        	LCD_Cursor(1);
+        	LCD_WriteData('C');
+        	break;
+      	case 'D':
+        	LCD_ClearScreen(); 
+        	LCD_Cursor(1);
+        	LCD_WriteData('D');
+        	break;
+      	case '*':
+        	LCD_ClearScreen(); 
+        	LCD_Cursor(1);
+        	LCD_WriteData('*');
+        	break;
+      	case '#':
+        	LCD_ClearScreen(); 
+        	LCD_Cursor(1);
+        	LCD_WriteData('#');
+        	break;
+      	case '0':
+        	LCD_ClearScreen(); 
+        	LCD_Cursor(1);
+        	LCD_WriteData('0');
+        	break;
+    	}
+    	break;
+    case pausebutton_release:
+    	break;
+    default:
+    	break;
   }
   return state;
  }
@@ -153,9 +153,7 @@ int main(void) {
       
     static task task1;
     task *tasks[] = {&task1};
-    const unsigned short numTasks = sizeof(tasks)/sizeof(tasks*);    
-    LCD_init();
-    LCD_ClearScreen();
+    const unsigned short numTasks = sizeof(tasks)/sizeof(task*);    
     
     task1.state = pauseButton_wait;
     task1.period = 1;
